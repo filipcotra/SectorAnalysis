5885b621b80206bea7736fb48c1e72e9f8aec841
FCotra <theheadlesswaffle@gmail.com>
2025-01-30 00:24:27 -0500

    Added Probability Reporting
    - For each AA, three files are output: raw counts, probability of AA contacts per sector, and probability of sector contacts per AA

--- printSectorInfo.py
+++ printSectorInfo.py
@@ -6,6 +6,16 @@ HEADER = f"#AA\tS1\tS2\tS3\tS4\tS5\tS6\tS7\tS8\tS9\tS10\ts11\tS12\n"
 COL_NAMES = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12"]
 numCols = len(COL_NAMES)
 
+# Simple function to write to file.
+# Parameters:
+#   df = The data frame to be written.
+#   fileName = A string representing the name of the file to be written to.
+def writeDf(df, fileName):
+    outputFile = open(file = fileName, mode = "w")
+    outputFile.write(HEADER)
+    outputFile.write(df.to_csv(sep = '\t', header = False))
+    outputFile.close()
+
 # This function will take a dictionary and print out all of its information
 # into separate files. I am aiming to make it output a computationally friendly
 # file format.
@@ -14,10 +24,8 @@ numCols = len(COL_NAMES)
 #   dictType = The name of the dictionary (bb2bb, bb2sc, etc.).
 def printSectorInfo(sectorDict, dictName):
     for parCode in sectorDict.keys():
-        # Making a file name.
-        fileName = f"{parCode}_{dictName}.txt"
         # Initializing a data frame with no rows.
-        printDf = pd.DataFrame(columns = COL_NAMES)
+        countDf = pd.DataFrame(columns = COL_NAMES)
         # Gathering information.
         sectorContacts_obj = sectorDict[parCode]
         sectorContacts_sectors = sectorContacts_obj.sectors
@@ -27,13 +35,21 @@ def printSectorInfo(sectorDict, dictName):
             for AA in sectorAAs.keys():
                 contactCount = sectorAAs[AA]
                 try:
-                    printDf.loc[AA, sector] = contactCount
+                    countDf.loc[AA, sector] = contactCount
                 except:
-                    printDf.loc[AA] = [0] * numCols
-                    printDf.loc[AA, sector] = contactCount
-        # Outputting to file.
-        printDf = printDf.fillna(0)
-        outputFile = open(file = fileName, mode = "w")
-        outputFile.write(HEADER)
-        outputFile.write(printDf.to_csv(sep = '\t', header = False))
-        outputFile.close()
+                    countDf.loc[AA] = [0] * numCols
+                    countDf.loc[AA, sector] = contactCount
+        # Making a file names.
+        countFileName = f"{parCode}.{dictName}.counts.txt"
+        sectorProbFileName = f"{parCode}.{dictName}.sectorProbs.txt"
+        AAProbFileName = f"{parCode}.{dictName}.AAProbs.txt"
+        # Last preparations for writing data frames.
+        countDf = countDf.fillna(0)
+        sectorProbDf = countDf.div(countDf.sum(axis = 0), axis = 1) # Probability distribution for each sector
+        sectorProbDf = sectorProbDf.fillna(0)
+        AAProbDf = countDf.div(countDf.sum(axis = 1), axis = 0) # Probability distribution for each contacting AA
+        AAProbDf = AAProbDf.fillna(0)
+        # Writing frames.
+        writeDf(countDf, countFileName)
+        writeDf(sectorProbDf, sectorProbFileName)
+        writeDf(AAProbDf, AAProbFileName)
